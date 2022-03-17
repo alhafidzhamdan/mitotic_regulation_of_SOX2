719fa6b4d8886ccffca9a8315dd7e76512027e73
alhafidzhamdan <53702866+alhafidzhamdan@users.noreply.github.com>
2022-03-17 22:40:21 +0000

    Update submit_bwa.sh

--- scripts/submit_bwa.sh
+++ scripts/submit_bwa.sh
@@ -19,12 +19,12 @@
 
 CONFIG=$1
 IDS=$2
+READ_DIR=$3
+BAM_DIR=$4
 
 source $CONFIG
 
 SAMPLE_ID=`head -n $SGE_TASK_ID $IDS | tail -n 1 | cut -f 8`
-READ_DIR=/exports/igmm/eddie/Glioblastoma-WGS/ChIP-seq/H3K27ac-seq_GBM/reads/Paired_end_reads
-BAM_DIR=/exports/igmm/eddie/Glioblastoma-WGS/ChIP-seq/H3K27ac-seq_GBM/bams
 
 ## Index ref genome already downloaded.
 ## Align fastq reads 
@@ -32,15 +32,6 @@ BAM_DIR=/exports/igmm/eddie/Glioblastoma-WGS/ChIP-seq/H3K27ac-seq_GBM/bams
 ##if paired-end reads
 cd $READ_DIR
 echo "Aligning fastq file for ${SAMPLE_ID} ... "
-bwa mem -M -t 20 $BWA_REF ${SAMPLE_ID}_1.fastq.gz ${SAMPLE_ID}_2.fastq.gz | samtools sort -@ 10 - -T ${SAMPLE_ID} -o $BAM_DIR/${SAMPLE_ID}.sorted.bam
+bwa mem -M -t 20 $BWA_REF ${SAMPLE_ID}_R1_001.fastq.gz ${SAMPLE_ID}_R2_001.fastq.gz | samtools sort -@ 10 - -T ${SAMPLE_ID} -o $BAM_DIR/${SAMPLE_ID}.sorted.bam
 samtools index $BAM_DIR/${SAMPLE_ID}.sorted.bam
 samtools flagstat $BAM_DIR/${SAMPLE_ID}.sorted.bam > $BAM_DIR/${SAMPLE_ID}.stats.out
-
-## Align, sort, index, and statistics.
-##cd $READ_DIR
-##echo "Aligning fastq file for ${SAMPLE_ID} ... "
-##bwa mem -M -t 20 $BWA_REF ${SAMPLE_ID}.fastq.gz | samtools sort -@ 10 - -T ${SAMPLE_ID} -o $BAM_DIR/${SAMPLE_ID}.sorted.bam
-##samtools index $BAM_DIR/${SAMPLE_ID}.sorted.bam
-##samtools flagstat $BAM_DIR/${SAMPLE_ID}.sorted.bam > $BAM_DIR/${SAMPLE_ID}.stats.out
-
-
