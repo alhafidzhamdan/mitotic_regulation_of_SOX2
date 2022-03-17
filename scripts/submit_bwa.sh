236d701c4496352161d3a8827c76588c6b050607
alhafidzhamdan <53702866+alhafidzhamdan@users.noreply.github.com>
2022-03-17 22:40:54 +0000

    Update submit_bwa.sh

--- scripts/submit_bwa.sh
+++ scripts/submit_bwa.sh
@@ -5,9 +5,6 @@
 #
 # CONFIG is the path to the file scripts/config.sh which contains environment variables set to
 # commonly used paths and files in the script
-# IDS is a list of sample ids, one per line, where tumor and normal samples are designated by
-# the addition of a T or an N to the sample id.
-# TYPE is either sample name or input
 #
 #$ -N BWA
 #$ -j y
@@ -26,10 +23,7 @@ source $CONFIG
 
 SAMPLE_ID=`head -n $SGE_TASK_ID $IDS | tail -n 1 | cut -f 8`
 
-## Index ref genome already downloaded.
-## Align fastq reads 
 
-##if paired-end reads
 cd $READ_DIR
 echo "Aligning fastq file for ${SAMPLE_ID} ... "
 bwa mem -M -t 20 $BWA_REF ${SAMPLE_ID}_R1_001.fastq.gz ${SAMPLE_ID}_R2_001.fastq.gz | samtools sort -@ 10 - -T ${SAMPLE_ID} -o $BAM_DIR/${SAMPLE_ID}.sorted.bam
