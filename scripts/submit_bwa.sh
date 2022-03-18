c3cc1e71f8c1d4f0dff11d3a9bd6bcbf9548b089
alhafidzhamdan <53702866+alhafidzhamdan@users.noreply.github.com>
2022-03-18 11:40:04 +0000

    Update submit_bwa.sh

--- scripts/submit_bwa.sh
+++ scripts/submit_bwa.sh
@@ -26,6 +26,6 @@ SAMPLE_ID=`head -n $SGE_TASK_ID $IDS | tail -n 1 | cut -f 8`
 
 cd $READ_DIR
 echo "Aligning fastq file for ${SAMPLE_ID} ... "
-bwa mem -M -t 20 $BWA_REF ${SAMPLE_ID}_R1_001.fastq.gz ${SAMPLE_ID}_R2_001.fastq.gz | samtools sort -@ 10 - -T ${SAMPLE_ID} -o $BAM_DIR/${SAMPLE_ID}.sorted.bam
+bwa mem -M -t 20 $BWA_REF ${SAMPLE_ID}_R1.fastq.gz ${SAMPLE_ID}_R2.fastq.gz | samtools sort -@ 10 - -T ${SAMPLE_ID} -o $BAM_DIR/${SAMPLE_ID}.sorted.bam
 samtools index $BAM_DIR/${SAMPLE_ID}.sorted.bam
 samtools flagstat $BAM_DIR/${SAMPLE_ID}.sorted.bam > $BAM_DIR/${SAMPLE_ID}.stats.out
